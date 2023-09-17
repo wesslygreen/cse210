f8ce544624d4146a796dae890f631fc1ae116bad
wesslygreen <gre21008@byui.edu>
2023-09-16 20:10:22 -0600

    prep 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
-        Console.Write("What is your last name?");
+        Console.Write("What is your last name? ");
         string lastname = Console.ReadLine();
         Console.WriteLine($"Your name is {lastname}, {firstName} {lastname}.");
 
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        int userGrade = int.Parse(Console.ReadLine());
+        string letterGrade;
+
+        if (userGrade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (userGrade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (userGrade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (userGrade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else 
+        {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"You got a {letterGrade}.");
+
+
+        if (letterGrade == "A" || letterGrade == "B" || letterGrade == "C")
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else 
+        {
+            Console.WriteLine("Try harder next time.");
+        }
+
+
+
     }
+    
 }
