57b4d8b561983433c14de9f87adc96810bdd2e0d
wesslygreen <gre21008@byui.edu>
2023-09-16 19:20:46 -0600

    prep one

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name?");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {firstName} {lastname}.");
+
+
+
     }
 }
+
